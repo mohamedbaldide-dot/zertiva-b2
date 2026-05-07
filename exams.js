9a1f19a62f3ad899dd3ad2f389f5e8282d2b413e
mohamedbaldide-dot <mohamedbaldi.de@gmail.com>
2026-05-07 04:30:40 +0100

    Update exams.js

--- exams.js
+++ exams.js
@@ -60,7 +60,7 @@ function showLockedModalForExam(examTitle) {
     modal.id = 'globalLockedModal';
     modal.style.cssText = `
         position: fixed; top: 0; left: 0; width: 100%; height: 100%;
-        background: rgba(0,0,0,0.75); z-index: 999999;
+        background: rgba(0,0,0,0.08); z-index: 999999;
         display: flex; justify-content: center; align-items: center;
         direction: rtl;
     `;
